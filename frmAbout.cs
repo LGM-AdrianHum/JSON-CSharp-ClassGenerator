0359a8c13146dfd2dea51cb4ac2755b4ef5159ee
Adrian Hum <a@littlegreenmartian.net>
2016-12-15 20:03:44 +1100

    Initial Drop. Converted from 3.5 to 4.5.1

---
+++ frmAbout.cs
@@ -0,0 +1,49 @@
+﻿// Copyright © 2010 Xamasoft
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Xamasoft.JsonClassGenerator.UI
+{
+    [SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
+    public partial class FrmAbout : Form
+    {
+        public FrmAbout()
+        {
+            InitializeComponent();
+#if !APPSERVICES
+            btnSendFeedback.Visible = false;
+            btnCheckUpdates.Visible = false;
+#endif
+            Font = SystemFonts.MessageBoxFont;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.xamasoft.com/json-class-generator");
+        }
+
+        private void frmAbout_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = string.Format(lblVersion.Text, Assembly.GetExecutingAssembly().GetName().Version);
+        }
+
+        private void btnSendFeedback_Click(object sender, EventArgs e)
+        {
+#if APPSERVICES
+            Program.appServices.ShowFeedbackForm(this);
+#endif
+        }
+
+        private void btnCheckUpdates_Click(object sender, EventArgs e)
+        {
+#if APPSERVICES
+            Program.appServices.UpdateChecker.ManualUpdatesCheck(this);
+#endif
+        }
+    }
+}
---
+++ frmClassGenerator.cs
@@ -0,0 +1,343 @@
+﻿// Copyright © 2010 Xamasoft
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Xamasoft.JsonClassGenerator.CodeWriters;
+using Xamasoft.JsonClassGenerator.UI.Properties;
+
+namespace Xamasoft.JsonClassGenerator.UI
+{
+    public partial class FrmCSharpClassGeneration : Form
+    {
+        private static readonly object[] CodeWriters =
+        {
+            new CSharpCodeWriter(),
+            new VisualBasicCodeWriter(),
+            new TypeScriptCodeWriter()
+            //  new JavaCodeWriter()
+        };
+
+        private string _lastGeneratedString;
+
+
+        public FrmCSharpClassGeneration()
+        {
+            InitializeComponent();
+            // ReSharper disable once VirtualMemberCallInConstructor
+            Font = SystemFonts.MessageBoxFont;
+
+            Program.InitAppServices();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var b = new FolderBrowserDialog())
+            {
+                b.ShowNewFolderButton = true;
+                b.SelectedPath = edtTargetFolder.Text;
+                b.Description = Resources.FrmCSharpClassGeneration_btnBrowse_Click_Please_select_a_folder_where_to_save_the_generated_files_;
+                if (b.ShowDialog(this) == DialogResult.OK)
+                    edtTargetFolder.Text = b.SelectedPath;
+            }
+        }
+
+        private void frmCSharpClassGeneration_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var settings = Settings.Default;
+            settings.UseProperties = radProperties.Checked;
+            settings.InternalVisibility = radInternal.Checked;
+            settings.SecondaryNamespace = edtSecondaryNamespace.Text;
+
+            if (radNestedClasses.Checked) settings.NamespaceStrategy = 0;
+            else if (radSameNamespace.Checked) settings.NamespaceStrategy = 1;
+            else settings.NamespaceStrategy = 2;
+
+            if (!settings.UseSeparateNamespace)
+                settings.SecondaryNamespace = string.Empty;
+            settings.Language = cmbLanguage.SelectedItem.GetType().Name;
+            settings.SingleFile = chkSingleFile.Checked;
+            settings.DocumentationExamples = chkDocumentationExamples.Checked;
+            settings.Save();
+        }
+
+        private void frmCSharpClassGeneration_Load(object sender, EventArgs e)
+        {
+            var settings = Settings.Default;
+            (settings.UseProperties ? radProperties : radFields).Checked = true;
+            (settings.InternalVisibility ? radInternal : radPublic).Checked = true;
+            edtSecondaryNamespace.Text = settings.SecondaryNamespace;
+            if (settings.NamespaceStrategy == 0) radNestedClasses.Checked = true;
+            else if (settings.NamespaceStrategy == 1) radSameNamespace.Checked = true;
+            else radDifferentNamespace.Checked = true;
+            cmbLanguage.Items.AddRange(CodeWriters);
+            var langIndex = CodeWriters.ToList().FindIndex(x => x.GetType().Name == settings.Language);
+            cmbLanguage.SelectedIndex = langIndex != -1 ? langIndex : 0;
+            chkSingleFile.Checked = settings.SingleFile;
+            chkDocumentationExamples.Checked = settings.DocumentationExamples;
+            UpdateStatus();
+        }
+
+        private void edtNamespace_TextChanged(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            HideCompletionMessage();
+            if (edtTargetFolder.Text == string.Empty)
+            {
+                MessageBox.Show(this, Resources.SpecifyOutputDirectory, Text, MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+            if (edtNamespace.Text == string.Empty)
+            {
+                MessageBox.Show(this, Resources.SpecifyNamespace, Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var gen = Prepare();
+            if (gen == null) return;
+            try
+            {
+                gen.GenerateClasses();
+                messageTimer.Stop();
+                lblDone.Visible = true;
+                lnkOpenFolder.Visible = true;
+                messageTimer.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, Resources.CantGenerateCode + ex.Message, Text, MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void PasteAndGo()
+        {
+            HideCompletionMessage();
+            var jsonClipboard = Clipboard.GetText(format: TextDataFormat.UnicodeText);
+            if (jsonClipboard != _lastGeneratedString)
+            {
+                var jsonClipboardTrimmed = jsonClipboard.Trim();
+                var jsonTextboxTrimmed = edtJson.Text.Trim();
+                if (
+                    jsonClipboardTrimmed.StartsWith("{") || jsonClipboardTrimmed.StartsWith("[") ||
+                    !(jsonTextboxTrimmed.StartsWith("{") || jsonTextboxTrimmed.StartsWith("[")))
+                    edtJson.Text = jsonClipboard;
+            }
+            var gen = Prepare();
+            if (gen == null) return;
+            try
+            {
+                gen.TargetFolder = null;
+                gen.SingleFile = true;
+                using (var sw = new StringWriter())
+                {
+                    gen.OutputStream = sw;
+                    gen.GenerateClasses();
+                    sw.Flush();
+                    _lastGeneratedString = sw.ToString();
+                    Clipboard.SetText(_lastGeneratedString);
+                }
+                messageTimer.Stop();
+                lblDoneClipboard.Visible = true;
+                messageTimer.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, Resources.CantGenerateCode + ex.Message, Text, MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+
+        private JsonClassGenerator Prepare()
+        {
+            if (edtJson.Text == string.Empty)
+            {
+                MessageBox.Show(this, Resources.GiveJsonSample, Text, MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                edtJson.Focus();
+                return null;
+            }
+
+
+            if (edtMainClass.Text == string.Empty)
+            {
+                MessageBox.Show(this, Resources.SpecifyMainClass, Text, MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return null;
+            }
+
+            var gen = new JsonClassGenerator
+            {
+                Example = edtJson.Text,
+                InternalVisibility = radInternal.Checked,
+                CodeWriter = (ICodeWriter) cmbLanguage.SelectedItem
+            };
+            gen.ExplicitDeserialization = chkExplicitDeserialization.Checked && gen.CodeWriter is CSharpCodeWriter;
+            gen.Namespace = string.IsNullOrEmpty(edtNamespace.Text) ? null : edtNamespace.Text;
+            gen.NoHelperClass = chkNoHelper.Checked;
+            gen.SecondaryNamespace = radDifferentNamespace.Checked && !string.IsNullOrEmpty(edtSecondaryNamespace.Text)
+                ? edtSecondaryNamespace.Text
+                : null;
+            gen.TargetFolder = edtTargetFolder.Text;
+            gen.UseProperties = radProperties.Checked;
+            gen.MainClass = edtMainClass.Text;
+            gen.UsePascalCase = chkPascalCase.Checked;
+            gen.UseNestedClasses = radNestedClasses.Checked;
+            gen.ApplyObfuscationAttributes = chkApplyObfuscationAttributes.Checked;
+            gen.SingleFile = chkSingleFile.Checked;
+            gen.ExamplesInDocumentation = chkDocumentationExamples.Checked;
+            return gen;
+        }
+
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            using (var w = new FrmAbout())
+            {
+                w.ShowDialog(this);
+            }
+        }
+
+        private void chkExplicitDeserialization_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void UpdateStatus()
+        {
+            if (edtSecondaryNamespace.Text.Contains("JsonTypes") || (edtSecondaryNamespace.Text == string.Empty))
+                edtSecondaryNamespace.Text = edtNamespace.Text == string.Empty
+                    ? string.Empty
+                    : edtNamespace.Text + "." + edtMainClass.Text + "JsonTypes";
+
+            edtSecondaryNamespace.Enabled = radDifferentNamespace.Checked;
+
+            var writer = (ICodeWriter) cmbLanguage.SelectedItem;
+
+            chkPascalCase.Enabled = !(writer is TypeScriptCodeWriter);
+            chkExplicitDeserialization.Enabled = writer is CSharpCodeWriter;
+            chkNoHelper.Enabled = chkExplicitDeserialization.Enabled && chkExplicitDeserialization.Checked;
+        }
+
+        private void radNestedClasses_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void radSameNamespace_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void radDifferentNamespace_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void edtMainClass_TextChanged(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void cmbLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                e.Handled = true;
+                using (var f = new FrmAbout())
+                {
+                    f.ShowDialog(this);
+                }
+            }
+            else if (e.KeyCode == Keys.F9)
+            {
+                e.Handled = true;
+                PasteAndGo();
+            }
+            base.OnKeyDown(e);
+        }
+
+        private void messageTimer_Tick(object sender, EventArgs e)
+        {
+            messageTimer.Stop();
+            HideCompletionMessage();
+        }
+
+        private void HideCompletionMessage()
+        {
+            lnkOpenFolder.Visible = false;
+            lblDone.Visible = false;
+            lblDoneClipboard.Visible = false;
+        }
+
+        private void lnkOpenFolder_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                Process.Start(edtTargetFolder.Text);
+            }
+            catch (Exception)
+            {
+
+                //IGNORE
+            }
+        }
+
+        private void edtJson_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && (e.KeyCode == Keys.A))
+            {
+                edtJson.SelectionStart = 0;
+                edtJson.SelectionLength = edtJson.TextLength;
+                e.Handled = true;
+            }
+        }
+
+        private void btnPasteAndGo_Click(object sender, EventArgs e)
+        {
+            PasteAndGo();
+        }
+
+        //}
+        //    edtJson.SelectAll();
+        //{
+
+        //private void edtJson_Enter(object sender, EventArgs e)
+        //}
+        //    edtNamespace.SelectAll();
+        //{
+
+        //private void edtNamespace_Enter(object sender, EventArgs e)
+        //}
+        //    edtTargetFolder.SelectAll();
+        //{
+
+        //private void edtTargetFolder_Enter(object sender, EventArgs e)
+        //}
+        //    edtMainClass.SelectAll();
+
+        //    edtMainClass.Focus();
+        //{
+
+        //private void edtMainClass_Enter(object sender, EventArgs e)
+    }
+}
